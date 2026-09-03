47e5ec21afd1ee3167464499a98871d908727914
nntan041299 <nntan041299@gmail.com>
2026-09-03 21:36:05 +0700

    Update the rest endpoints

--- bakery-ui/src/rest/endpoint.ts
+++ bakery-ui/src/rest/endpoint.ts
@@ -1,13 +1,13 @@
 export const ENDPOINT = {
-  LOGIN: "bakery-service/api/auth/login",
-  GOOGLE_LOGIN: "bakery-service/api/auth/google/login",
-  GOOGLE_LOGIN_GET_TOKEN: "bakery-service/api/auth/google/getToken",
-  USERS: "bakery-service/api/users/current-user",
-  SIGN_UP: "bakery-service/api/auth/register",
-  SIGN_OUT: "bakery-service/api/auth/logout",
-  REFRESH_TOKEN: "bakery-service/api/auth/refresh",
-  STORES: "bakery-service/api/stores",
-  MY_STORES: "bakery-service/api/stores/mine",
+  LOGIN: "auth/login",
+  GOOGLE_LOGIN: "auth/google/login",
+  GOOGLE_LOGIN_GET_TOKEN: "auth/google/getToken",
+  USERS: "users/current-user",
+  SIGN_UP: "auth/register",
+  SIGN_OUT: "auth/logout",
+  REFRESH_TOKEN: "auth/refresh",
+  STORES: "stores",
+  MY_STORES: "stores/mine",
 } as const;
 
 export const storeProductsEndpoint = (storeId: string | number): string =>
